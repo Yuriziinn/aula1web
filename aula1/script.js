e2f9adbd2159cd870887e46ce73082241c0aad2e
Yurizin <yuri.pleyerksks@gmail.com>
2026-09-21 22:19:45 -0300

    Add files via upload

---
+++ aula1/script.js
@@ -0,0 +1,29 @@
+
+
+console.log('to certo')
+alert('Atencão! misao de rank S recebida')
+
+const aceitou = confirm('Vc aceita a missão de resgatar o Sasuke?')
+console.log('Missao aceita: ', aceitou);
+
+const nomeNinja = prompt('Qual o seu nome de ninja para o registro?');
+alert('Bem-vindo à aliança Shinobi, ' + nomeNinja + '!')
+
+const jutsu = prompt('Qual seu jutsu favorito?');
+alert('Executando ' + jutsu +' agora !');
+
+const nome = prompt("Qual o seu nome? ");
+console.log("Seja bem vindo ao teste Chunnin, " , nome)
+
+const comida = confirm("Vc prefere ramem ou Sushi?")
+console.log("Eu também gosto de ", comida)
+
+const ninja1 = prompt('digite seu nome para formamos as equipes: ', ninja1)
+const ninja2 = prompt('digite seu nome para formamos as equipes: ', ninja2)
+const ninja3 = prompt('digite seu nome para formamos as equipes: ', ninja3)
+
+console.log('Time 7 formado com: ', ninja1, ',', ninja2, 'e' , ninja3)
+
+const nomedoNinja = prompt("digite o nome do ninja: ");
+const relatorio = prompt("ninja digite o seu relatorio: ");
+console.log("De ", nomedoNinja , "| Para : Hokage | Mensagem : ", relatorio)
